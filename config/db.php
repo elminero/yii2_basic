c98c2f4027255664ccf3f1a00955c4da257d52e5
Robert Farber <elminero@cox.net>
2015-04-14 21:09:33 +0000

    MySQL Workbench configuration notes added to config/db.php

--- config/db.php
+++ config/db.php
@@ -7,3 +7,29 @@ return [
     'password' => '',
     'charset' => 'utf8',
 ];
+
+/*
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=contact',
+        'username' => 'ian',
+        'password' => 'super1964',
+        'charset' => 'utf8',
+    ];
+
+    MySQL Workbench for use with Vagrant
+
+    SSH Hostname: 127.0.0.1:2222
+
+    SSH Username: vagrant
+
+    SSH Key File: C:\vag\web\puphpet\files\dot\ssh\id_rsa
+
+    MySQL Server Port: 3306
+
+    Username: root
+
+Password when asked: 123
+
+
+*/
