1a30a4165d51554a2f7e3dfef0cf552796cad9cf
Robert Farber <elminero@cox.net>
2015-04-15 01:46:36 +0000

    Added notes to site index

--- views/site/index.php
+++ views/site/index.php
@@ -16,32 +16,51 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>composer.phar</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Download the composer.phar executable or use the installer.</p>
+                <p>$ curl -sS https://getcomposer.org/installer | php</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>MySQL Workbench for use with Vagrant</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>
+                    SSH Hostname: 127.0.0.1:2222<br />
+
+                    SSH Username: vagrant<br />
+
+                    SSH Key File: C:\vag\web\puphpet\files\dot\ssh\id_rsa<br />
+
+                    MySQL Server Port: 3306<br />
+
+                    Username: root<br />
+
+                    Password when asked: 123<br />
+                </p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Using Gii with Vagrant</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>
+                    config/web.php<br />
+
+                    if (YII_ENV_DEV) {<br />
+                    // configuration adjustments for 'dev' environment<br />
+                    $secure = ['allowedIPs' => ['*']];<br />
+
+                    $config['bootstrap'][] = 'debug';<br />
+                    $config['modules']['debug'] = ['class' => 'yii\debug\Module'] + $secure;<br />
+
+                    $config['bootstrap'][] = 'gii';<br />
+                    $config['modules']['gii'] = ['class' => 'yii\gii\Module'] + $secure;<br />
+                    }<br />
+
+
+                </p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
